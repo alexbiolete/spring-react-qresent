14df311f5cd13c57c5e8c837aa17b7712f818df2
Alex <alexbiolete@pm.me>
2021-10-22 09:59:19 +0300

    Created Spring project for the server

---
+++ server/src/test/java/com/mps/team3/qresent/QresentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mps.team3.qresent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QresentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
