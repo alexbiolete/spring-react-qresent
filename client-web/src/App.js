bbd9a1ff5b803c9058430b9ad5abfa897af7199b
Alex <alexbiolete@pm.me>
2021-10-22 10:30:20 +0300

    Created React.js project with Sass, CRACO and TailwindCSS for the web client

---
+++ client-web/src/App.js
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    // <div className="dark">
+    <div>
+      <div className="min-h-screen flex flex-between p-4 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-200">
+        <div className="max-w-7xl w-full mx-auto">
+          <header className="w-full">
+            <div className="w-full flex justify-center px-6 py-4 rounded-2xl bg-gray-200 dark:bg-gray-800">
+              <h1 className="font-light text-2xl tracking-wide">
+                QResent
+              </h1>
+            </div>
+          </header>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client-web/tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      gray: colors.trueGray
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
