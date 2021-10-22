38afc63f229a441a6de623fcfe50905b9b4171f9
Alex Biolete <alexbiolete@pm.me>
2021-10-22 10:54:09 +0300

    Added Spring Web, Spring Data JPA and MySQL Driver, configured MySQL connection and changed main application class name

--- server/src/main/java/com/mps/team3/qresent/Application.java
+++ server/src/main/java/com/mps/team3/qresent/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QresentApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QresentApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
