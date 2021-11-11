9af344b91d8020efb0f408d958770f5f491f8530
Teodora <you@example.com>
2021-11-11 20:59:39 +0200

    signup+login FE

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -1,29 +1,29 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './views/Home';
-import NotFound from './views/NotFound';
-import Login from './views/Login';
-import Signup from './views/Signup';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap'
-import { default as PublicLayout } from './layouts/Public';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { default as PublicLayout } from "./layouts/Public";
 
 const App = () => {
-    return (
-                <BrowserRouter>
-                    <PublicLayout>
-                        <Switch>
-                            <Route exact path="/">
-                                <Home />
-                            </Route>
-                            <Route exact path="/login">
-                                <Login />
-                            </Route>
-                            <Route exact path="/signup">
-                                <Signup />
-                            </Route>
-                        </Switch>
-                    </PublicLayout>
-                </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <PublicLayout>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </PublicLayout>
+    </BrowserRouter>
   );
 };
 
--- client-web/src/views/Signup.js
+++ client-web/src/views/Signup.js
@@ -1,52 +1,71 @@
 import React, { Component, useImperativeHandle, useRef, useState } from "react";
-import { Form, Button, Card, Row, Col, Container } from 'react-bootstrap';
-import axios from 'axios';
+import { Form, Button, Card, Row, Col, Container } from "react-bootstrap";
+import axios from "axios";
 
 export default function Signup() {
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = () => {
-        console.log(username)
-    }
+  const handleSubmit = () => {
+    const user = {
+      email: email,
+      username: username,
+      password: password,
+    };
 
-    const componentDidMount = () => {
-        const user = {
-            email: '',
-            username: '',
-            password: '',
-        };
+    axios.post("http://localhost:3000/users/all", user).then((response) => console.log(user));
+    //console.log(user)
+  };
 
-        axios.post('http://localhost:3000/users/all', user).then(response => this.setState({ email: user.email }));
-    }
-
-    return (
-        <>
-        <Container>
-                <Row>
-                <Col lg={6} md={6} sm={12} className="m-auto p-5">
-                    <div className="LoginBox p-5">
-                <Form>
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col lg={6} md={6} sm={12} className="m-auto p-5">
+            <div className="LoginBox p-5">
+              <Form>
                 <h1 className="text-center mt-2 p-5">Signup</h1>
-                    <Form.Group id='email'>
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type='email' value={email} onChange={e => setEmail(e.target.value)} required />
-                    </Form.Group>
-                    <Form.Group id='username'>
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type='username' value={username} onChange={e => setUsername(e.target.value)} required />
-                    </Form.Group>
-                    <Form.Group id='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' value={password} onChange={e => setPassword(e.target.value)} required />
-                    </Form.Group>
-                    <Button className="w-100 text-center mt-4" type="submit" href="/login" onClick={ ()=> handleSubmit() } >Signup</Button>
-                </Form>
-                </div>
-                </Col>
-                </Row>
-                </Container>
-        </>
-    )
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="username">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+                <Button
+                  className="w-100 text-center mt-4"
+                  type="submit"
+                  href="/login"
+                  onClick={() => handleSubmit()}
+                >
+                  Signup
+                </Button>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
