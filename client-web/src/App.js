f04311327ac099ae0dadabe28a359bc4d15e8d11
Monica Hariton <monica.hariton13@gmail.com>
2021-11-17 20:11:59 +0200

    dashboard wip

--- client-web/src/App.js
+++ client-web/src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './views/Home';
 import NotFound from './views/NotFound';
 import { default as PublicLayout } from './layouts/Public';
+import DashboardProfesor from './views/DashboardProfesor';
+import DashboardStudent from './views/DashboardStudent';
+import DashboardAdmin from './views/DashboardAdmin';
+
 
 const App = () => {
   return (
@@ -9,6 +13,9 @@ const App = () => {
       <PublicLayout>
         <Switch>
           <Route path="/" component={Home} exact />
+          <Route path="/professor/dashboard" component={DashboardProfesor} exact />
+          <Route path="/student/dashboard" component={DashboardStudent} exact />
+          <Route path="/admin/dashboard" component={DashboardAdmin} exact />
           <Route path="*" component={NotFound} />
         </Switch>
       </PublicLayout>
