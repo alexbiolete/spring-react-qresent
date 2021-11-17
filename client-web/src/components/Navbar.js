a6994f45b7bdf14b7e987d66b72c41d3249758f3
Budi <blajdragos2000@gmail.com>
2021-11-17 20:56:44 +0200

    Change navbar

--- client-web/src/components/Navbar.js
+++ client-web/src/components/Navbar.js
@@ -14,18 +14,18 @@ const Navbar = () => {
       <nav className="fixed top-0 bg-white w-full h-16 border-t md:border-t-0 border-gray-100 shadow-sm flex items-center">
         <div className="w-full max-w-7xl mx-auto px-4 md:px-8 flex">
           <div className="flex-1 inline-flex items-center justify-begin">
-            <div className="flex-1 inline-flex items-center justify-begin">
+            <div className="flex-1 inline-flex md:hidden items-center justify-begin">
 
             </div>
-            <div className="flex-grow flex md:hidden items-center justify-center">
+            <div className="flex-grow flex items-center justify-center">
               <Logo className="w-36 h-12 fill-current" />
             </div>
             <div className="flex-1 hidden md:flex justify-between md:justify-evenly space-x-2 md:space-x-6">
               <NavbarTabs />
             </div>
             <div className="flex-1 inline-flex items-center justify-end">
-              <NavbarLink label="Log in" path="/" />
-              <NavbarLink label="Sign up" path="/" />
+              <NavbarLink label="Log in" path="/login" />
+              <NavbarLink label="Sign up" path="/signup" />
             </div>
           </div>
         </div>
