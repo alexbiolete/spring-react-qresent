ab84c905b3e0d56ab18d8a562a42a0bca04c0160
Alex <alexbiolete@pm.me>
2021-12-09 20:02:10 +0200

    Replaced Navbar Log in and Sign up buttons with a responsive Account button

--- client-web/src/components/organisms/Navbar.js
+++ client-web/src/components/organisms/Navbar.js
@@ -1,6 +1,6 @@
+import { Link } from "react-router-dom";
 import Logo from '../../resources/Logo';
 import NavbarTabs from '../molecules/NavbarTabs';
-import NavbarLink from '../atoms/NavbarLink';
 
 const Navbar = () => {
   return (
@@ -19,8 +19,22 @@ const Navbar = () => {
             </div>
             {/* TO DO: Responsive navbar menu */}
             <div className="flex-1 inline-flex items-center justify-end">
-              <NavbarLink label="Log in" path="/login" />
-              <NavbarLink label="Sign up" path="/signup" />
+              {/* <NavbarLink label="Log in" path="/login" />
+              <NavbarLink label="Sign up" path="/signup" /> */}
+              <Link
+                className="flex items-center space-x-1 focus:outline-none border-2 border-transparent md:hover:border-gray-200 px-2 py-1 rounded-full text-gray-600 hover:text-gray-700 transition ease-in-out duration-500"
+                to="/login"
+              >
+                <span className="bg-transparent rounded-full overflow-hidden">
+                  {/* ../Resources/svg/user.svg */}
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
+                  </svg>
+                </span>
+                <span className="hidden md:block font-medium text-sm">
+                  {'Account'}
+                </span>
+              </Link>
             </div>
           </div>
         </div>
